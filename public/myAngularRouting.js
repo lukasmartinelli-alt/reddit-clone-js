ff0faf0be84702db02008671186b98d2007b8916
Severin Bühler <sevi_buehler@hotmail.com>
2014-11-07 18:58:36 +0100

    ng-view implemented

---
+++ public/myAngularRouting.js
@@ -0,0 +1,25 @@
+var myApp = angular.module('myApp',['ngRoute']);
+
+myApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'view/table.html'
+                //   controller: 'ShowOrdersController'
+            }).
+            when('/new', {
+                templateUrl: 'view/new.html'
+            //   controller: 'AddOrderController'
+            }).
+            when('/entry', {
+                templateUrl: 'view/entry.html'
+                //   controller: 'ShowOrdersController'
+            }).
+            when('/404', {
+                templateUrl: 'view/404.html'
+                //   controller: 'ShowOrdersController'
+            }).
+            otherwise({
+                redirectTo: '/404'
+            });
+    }]);
