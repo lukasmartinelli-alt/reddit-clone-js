63b7b139ee5b55585898584f32d49a17e1a524e1
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-13 16:16:08 +0100

    Added login procedure

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -2,13 +2,24 @@ angular.module('redditclone', [])
 .controller('userController', ['$scope', '$http', function($scope, $http) {
     $scope.username = "";
     $scope.password = "";
+    $scope.loggedIn = false;
+
     $scope.login = function() {
         $http.post('/login', { "name": $scope.username, "password": $scope.password })
             .success(function(data, status, headers, config) {
-                console.log(data);
+                console.log("User " + $scope.username + " logged in");
+                $scope.loggedIn = true;
             })
             .error(function(data, status, headers, config) {
-                console.log("FAAAIL");
+                $scope.loggedIn = false;
             });
     };
+    $scope.register = function() {
+        $http.post('/register', { "name": $scope.username, "password": $scope.password })
+            .success(function(data, status, headers, config) {
+                console.log("User " + $scope.username + " registered");
+                $scope.login();
+            });
+    };
+
 }]);
