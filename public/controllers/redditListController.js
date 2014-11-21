86d6f498caf4c9eea1520706e133d508c87edf5e
Murthy10 <samuel.kurath@gmail.com>
2014-11-21 15:16:54 +0100

    leider beide an redditListController.js gearbeitet

--- public/controllers/redditListController.js
+++ public/controllers/redditListController.js
@@ -1,27 +1,6 @@
 redditclone.controller('redditListController', ['$scope', '$http', 'socketService', 'loginService', function($scope, $http, socketService, loginService) {
     $scope.reddits = [];
-    $scope.$http = $http;
-    $scope.update = function() {
-        this.$http.get('/entries')
-            .success(function(data, status, headers, config) {
-                console.log("Loaded " + data.length + " reddits");
-                // Add areCommentsVisible property
-                data.forEach(function(entry) {
-                    entry.areCommentsVisible = false;
-                    entry.setCommentsVisible = function (){
-                        this.areCommentsVisible = true;
-                    };
-                    entry.newCommentText = "";
-                    entry.submitNewComment = function() {
-                        console.log("New comment added: " + this.newCommentText);
-                        $http.post("/entry/" + entry.id + "/comment",{text:entry.newCommentText}).success(function(data, status, headers, config) {
-                            // this callback will be called asynchronously
-                            // when the response is available
-                            console.log("comment added");
-                        })
-                    };
 
-<<<<<<< HEAD
     $scope.upEntry = function(entryId) {
         if(loginService.loggedIn) {
             socketService.emit('upEntry', {eId: entryId, uId: loginService.user_id});
@@ -54,33 +33,31 @@ redditclone.controller('redditListController', ['$scope', '$http', 'socketServic
         $scope.reddits[reddit.id].comments[comment.id].ra = comment.rating.value;
     });
 
-    $http.get('/entries')
-        .success(function(data, status, headers, config) {
-            console.log("Loaded " + data.length + " reddits");
-            // Add areCommentsVisible property
-            data.forEach(function(entry) {
-                entry.areCommentsVisible = false;
-                entry.setCommentsVisible = function (){
-                    this.areCommentsVisible = true;
-                };
-                entry.newCommentText = "";
-                entry.submitNewComment = function() {
-                    console.log("New comment added: " + this.newCommentText);
-                    $http.post("/entry/" + entry.id + "/comment",{text:entry.newCommentText}).success(function(data, status, headers, config) {
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        console.log("comment added");
-                    })
-                };
-=======
+    $scope.$http = $http;
+    $scope.update = function() {
+        this.$http.get('/entries')
+            .success(function (data, status, headers, config) {
+                console.log("Loaded " + data.length + " reddits");
+                // Add areCommentsVisible property
+                data.forEach(function (entry) {
+                    entry.areCommentsVisible = false;
+                    entry.setCommentsVisible = function () {
+                        this.areCommentsVisible = true;
+                    };
+                    entry.newCommentText = "";
+                    entry.submitNewComment = function () {
+                        console.log("New comment added: " + this.newCommentText);
+                        $http.post("/entry/" + entry.id + "/comment", {text: entry.newCommentText}).success(function (data, status, headers, config) {
+                            // this callback will be called asynchronously
+                            // when the response is available
+                            console.log("comment added");
+                        })
+                    };
                 });
                 this.reddits = data;
                 console.log("Loaded2 " + this.reddits.length + " reddits");
             }.bind(this));
->>>>>>> cc670772b19a7a1001710f17521daae9690a0324
-
+        $scope.update($http);
     };
-
-    $scope.update($http);
 }]);
 
