88793d00baf1398936678cb664834a4be4359ff4
Severin Bühler <sevi_buehler@hotmail.com>
2014-11-26 12:45:16 +0100

    fixed rebase errors

--- public/controllers/redditListController.js
+++ public/controllers/redditListController.js
@@ -2,32 +2,35 @@
 redditclone.controller('redditListController', ['$scope', '$http', 'socketService', 'loginService', function($scope, $http, socketService, loginService) {
     $scope.reddits = [];
     $scope.commentAddedID = -1;
+    $scope.isLoggedIn = function() {
+        return loginService.loggedIn;
+    };
     function extendReddit(entry) {
         entry.areCommentsVisible = false;
         entry.setCommentsVisible = function (){
-            this.areCommentsVisible = true;
+            if(loginService.loggedIn === true) {
+                this.areCommentsVisible = true;
+            }
         };
         entry.newCommentText = "";
         entry.submitNewComment = function() {
             $scope.commentAddedID = entry.id;
             $http.post("/entry/" + entry.id + "/comment",{text:entry.newCommentText}).success(function(data, status, headers, config) {
                 console.log("comment added successfully: " + entry.newCommentText);
-
             })
         };
     };
     $scope.update = function() {
         $http.get('/entries').success(function(data, status, headers, config) {
-                console.log("Loaded " + data.length + " reddits");
-                // Add areCommentsVisible property
                 data.forEach(function(entry) {
                     extendReddit(entry);
                     if(entry.id === $scope.commentAddedID) {
-                        console.log("id = " + $scope.commentAddedID);
+                        entry.setCommentsVisible();
                         $scope.commentAddedID = -1;
                     }
                 });
                 $scope.reddits = data;
+                console.log("Loaded " + data.length + " reddits");
         });
 
     };
@@ -64,32 +67,7 @@ redditclone.controller('redditListController', ['$scope', '$http', 'socketServic
         $scope.reddits[reddit.id].comments[comment.id].ra = comment.rating.value;
     });
 
-    $scope.$http = $http;
-    $scope.update = function() {
-        this.$http.get('/entries')
-            .success(function (data, status, headers, config) {
-                console.log("Loaded " + data.length + " reddits");
-                // Add areCommentsVisible property
-                data.forEach(function (entry) {
-                    entry.areCommentsVisible = false;
-                    entry.setCommentsVisible = function () {
-                        this.areCommentsVisible = true;
-                    };
-                    entry.newCommentText = "";
-                    entry.submitNewComment = function () {
-                        console.log("New comment added: " + this.newCommentText);
-                        $http.post("/entry/" + entry.id + "/comment", {text: entry.newCommentText}).success(function (data, status, headers, config) {
-                            // this callback will be called asynchronously
-                            // when the response is available
-                            console.log("comment added");
-                        })
-                    };
-                });
-                this.reddits = data;
-                console.log("Loaded2 " + this.reddits.length + " reddits");
-            }.bind(this));
 
-    };
-    $scope.update($http);
+    $scope.update();
 }]);
 
