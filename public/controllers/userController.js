15393370987eb5267477702ccada507557976eca
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-27 19:26:47 +0100

    Added logout

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -2,21 +2,27 @@ redditclone.controller('userController', ['$scope', '$http', 'loginService',
  function($scope, $http, loginService) {
     $scope.loggedIn = false;
 
+    $scope.logout = function() {
+        loginService.logout();
+        $scope.loggedIn = false;
+        $scope.loggedIn = loginService.loggedIn;
+    };
+
     $scope.login = function() {
         loginService.login($scope.username, $scope.password, function() {
             $scope.loggedIn = loginService.loggedIn;
         });
     };
+
     $scope.register = function() {
         loginService.register($scope.username, $scope.password, function() {
             $scope.loggedIn = loginService.loggedIn;
         });
     };
 
-    if(sessionStorage.user) {
-        var user = sessionStorage.user;
-        $scope.username = user.username;
-        $scope.password = user.password;
+    if(sessionStorage.username) {
+        $scope.username = sessionStorage.username;
+        $scope.password = sessionStorage.password;
         $scope.login();
     }
 }]);
--- public/services/loginService.js
+++ public/services/loginService.js
@@ -2,13 +2,19 @@ redditclone.factory('loginService', ['$http', function($http) {
     return {
         loggedIn: false,
         user: { username: "", password: "" },
+        logout: function() {
+            this.loggedIn = false;
+            sessionStorage.username = "";
+            sessionStorage.password = "";
+        },
         login: function(username, password, callback) {
             $http.post('/login', { "name": username, "password": password })
             .success(function(data, status, headers, config) {
                 console.log("User " + username + " logged in");
                 this.loggedIn = true;
                 this.user = { username: username, password: password };
-                sessionStorage.user = this.user;
+                sessionStorage.username = username;
+                sessionStorage.password = password;
                 callback();
             }.bind(this))
             .error(function(data, status, headers, config) {
